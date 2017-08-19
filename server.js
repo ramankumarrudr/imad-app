bf4cc81026b7608ca32995f7adec6acbb7c6e31d
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-19 21:47:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.get('/articleName',function(req,res){
     res.send(createTemplate(articleOne));
 });
 
-
+app.get('/article-two',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','article-two.html'));
+});
 app.get('/article-three',function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
