ee96998085189f109d454ea049cb9b026ebb4652
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-19 23:08:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,3 +7,13 @@ var img=document.getElementById('madi')
 img.onclick = function(){
     img.style.marginLeft = '100px';
 }
+//move image for every 50 ms
+var img=document.getElementById('madi')
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function(){
+    var interval = setInterval(moveRight,50)
+}
