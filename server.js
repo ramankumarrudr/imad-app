4f713ce38b88cd2cd3c900f1a23ce0e554575b6b
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-19 16:38:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send('article-0ne is served');
+    res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send('article-0ne is served');
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
