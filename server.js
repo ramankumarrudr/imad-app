b04099245479cfc611d3d3447f6963d6c107fbf2
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-20 11:16:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,9 +55,9 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res){
+app.get('/submit-name',function(req,res){
 //Get the names from the request
-var name = req.params.name;
+var name = req.query.name;
 
 names.push(name);
 //JASON-JavaScript objects Notation (way of converting js objects into string)
