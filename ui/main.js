406a2fe580df7a5cf488b8ddb3ae551fd0dd4e74
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-20 07:27:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
 console.log('Loaded!');
-//change the text
+/*change the text
 var element = document.getElementById('main-text');
 element.innerHtml = 'New value';
-//move the image
-//var img=document.getElementById('madi')
-//img.onclick = function(){
-  //  img.style.marginLeft = '100px';
-//}
-//move image for every 50 ms
+move the image
 var img=document.getElementById('madi')
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}*/
+
+//move image for every 50 ms
+/*var img=document.getElementById('madi')
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
@@ -16,4 +17,17 @@ function moveRight(){
 }
 img.onclick = function(){
     var interval = setInterval(moveRight,50)
-}
+}*/
+
+var button = document.getElementById('counter');
+button.onclick = function(){
+   //Make a request to counter
+   
+   //capture the response
+   
+   //render the variable in correct span
+   
+   counter=counter+1;
+   var span = document.getElementById('count');
+   span.innerHtml = counter.toString();
+} ;
