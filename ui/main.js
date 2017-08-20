26f8dcf5f33b029af84548b36240f1d17e4bc823
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-20 08:32:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight,50)
 }*/
-
+var counter=0;
 var button = document.getElementById('counter');
 button.onclick = function(){
    //Make a request to counter
