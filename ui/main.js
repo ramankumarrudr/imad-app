f1054df896b4363b51157fcf5735e8361d860e37
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-20 08:50:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,16 +18,30 @@ function moveRight(){
 img.onclick = function(){
     var interval = setInterval(moveRight,50)
 }*/
+
 var counter=0;
 var button = document.getElementById('counter');
 button.onclick = function(){
    //Make a request to counter
-   
+   var request = new XMLHttpRequest();
    //capture the response
+   request.onreadystatechange = function() {
+   if(request.readyState === XMLHttpRequest.DONE){
+       //take some action
+       if (request.status===200){
+           var counter =request.responseText;
+           var span =document.getElementById('count');
+           span.innerHTML =counter.tostring();
+       }
+   } 
+   };
+   // make the request
+   request.open('GET','http://http://ramankumarrudr.imad.hasura-app.io/counter',true);
+   request.send(null);
    
    //render the variable in correct span
    
-   counter=counter+1;
+ /*  counter=counter+1;
    var span = document.getElementById('count');
-   span.innerHTML = counter.toString();
-} ;
+   span.innerHTML = counter.toString(); */
+};
