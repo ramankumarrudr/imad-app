d2c3e6d14d430ddc2469195e22c9ba96710dbea4
ramankumarrudr <30767481+ramankumarrudr@users.noreply.github.com>
2017-08-31 15:22:57 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ names.push(name);
 res.send(JSON.stringify(names));
 });
 
+app.get('/databaseproject_1',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'R.M.K_student_login', 'stud_index.html'));
 });
